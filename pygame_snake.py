3b62e19c3f22c12ebba93fbd23b5b8ed760362a1
Aleksandr <kisel.nf97@gmail.com>
2021-01-24 21:26:25 +0300

    first commit

---
+++ pygame_snake.py
@@ -0,0 +1,90 @@
+import pygame
+from pygame import mixer
+from random import randrange
+
+
+
+RES_W = 700
+RES_H = 600
+SIZE = 50
+
+x, y = randrange(SIZE, RES_W - SIZE, SIZE), randrange(SIZE, RES_H - SIZE, SIZE)
+apple = randrange(SIZE, RES_W - SIZE, SIZE), randrange(SIZE, RES_H - SIZE, SIZE)
+length = 1
+snake = [(x, y)]
+dx, dy = 0, 0
+fps = 45
+dirs = {'W': True, 'S': True, 'A': True, 'D': True, }
+score = 0
+speed_count, snake_speed = 0, 10
+
+pygame.init()
+mixer.init()
+eat_sound = mixer.Sound('eat.wav')
+bg = mixer.music.load('bg.ogg')
+mixer.music.set_volume(0.4)
+mixer.music.play(1)
+
+surface = pygame.display.set_mode([RES_W, RES_H])
+clock = pygame.time.Clock()
+font_score = pygame.font.SysFont('Arial', 26, bold=True)
+font_end = pygame.font.SysFont('Arial', 66, bold=True)
+img = pygame.image.load('bg.png').convert()
+
+def close_game():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+while True:
+    surface.blit(img, (0, 0))
+    # drawing snake, apple
+    [pygame.draw.rect(surface, pygame.Color('green'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake]
+    pygame.draw.rect(surface, pygame.Color('red'), (*apple, SIZE, SIZE))
+    # show score
+    render_score = font_score.render(f'SCORE: {score}', 1, pygame.Color('orange'))
+    surface.blit(render_score, (5, 5))
+    # snake movement
+    speed_count += 1
+    if not speed_count % snake_speed:
+        x += dx * SIZE
+        y += dy * SIZE
+        snake.append((x, y))
+        snake = snake[-length:]
+    # eating food
+    if snake[-1] == apple:
+        eat_sound.play()
+        apple = randrange(SIZE, RES_W - SIZE, SIZE), randrange(SIZE, RES_H - SIZE, SIZE)
+        length += 1
+        score += 1
+        snake_speed -= 1
+        snake_speed = max(snake_speed, 4)
+    # game over
+    if x < 0 or x > RES_W - SIZE or y < 0 or y > RES_H - SIZE or len(snake) != len(set(snake)):
+        while True:
+            render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
+            surface.blit(render_end, (RES_W // 2 - 200, RES_H // 3))
+            pygame.display.flip()
+            close_game()
+
+    pygame.display.flip()
+    clock.tick(fps)
+    close_game()
+    # controls
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        if dirs['W']:
+            dx, dy = 0, -1
+            dirs = {'W': True, 'S': False, 'A': True, 'D': True, }
+    elif key[pygame.K_s]:
+        if dirs['S']:
+            dx, dy = 0, 1
+            dirs = {'W': False, 'S': True, 'A': True, 'D': True, }
+    elif key[pygame.K_a]:
+        if dirs['A']:
+            dx, dy = -1, 0
+            dirs = {'W': True, 'S': True, 'A': True, 'D': False, }
+    elif key[pygame.K_d]:
+        if dirs['D']:
+            dx, dy = 1, 0
+            dirs = {'W': True, 'S': True, 'A': False, 'D': True, }
---
+++ snake2.py
@@ -0,0 +1,168 @@
+import random
+import time
+from collections import deque
+from tkinter import *
+
+from PIL import ImageTk, Image
+
+game_width = 700
+game_height = 600
+snake_item = 20
+start_x = 24
+start_y = 24
+snake_x_nav = 0
+snake_y_nav = 0
+snake_size = 5
+difficulty = {'Easy': 0.50, 'Medium': 0.15, 'Hard': 0.05}
+
+tk = Tk()
+tk.title('Snake on Python')
+canvas = Canvas(tk, width=game_width, height=game_height,
+                bd=0)
+# set bg
+image = ImageTk.PhotoImage(Image.open(r'C:\Users\User\Desktop\portfolio\snake\bg.png'))
+canvas.create_image(0, 0, anchor=NW, image=image)
+canvas.pack()
+tk.update()
+
+# infinite queue of colors
+colors = deque(["#640CAB", "#582781", "#3F046F", "#9240D5", "#A468D5", '#FFF500', '#BFBA30',
+                '#A69F00', '#FFF840', '#FFFA73'
+                ])
+
+# the snake takes the color of the eaten apple
+next_snake_color = deque(['white', 'white'])
+
+
+def pick_color_apple(colors):
+    temp = colors.popleft()
+    colors.append(temp)
+    global next_snake_color
+    next_snake_color.popleft()
+    next_snake_color.append(temp)
+    return temp
+
+
+class SnakeApple:
+
+    def __init__(self):
+        self.apple_color = pick_color_apple(colors)
+        self.coord_x = random.randrange(1, 35)
+        self.coord_y = random.randrange(1, 30)
+        self.apple = canvas.create_oval(self.coord_x * snake_item, self.coord_y * snake_item,
+                                        self.coord_x * snake_item + snake_item, self.coord_y * snake_item + snake_item,
+                                        fill=self.apple_color)
+
+    def eat_apple(self):
+        canvas.delete(self.apple)
+
+    def check_eat_apple(self, snake_coord_x, snake_coord_y):
+        if snake_coord_x == self.coord_x and snake_coord_y == self.coord_y:
+            return True
+
+
+class Snake:
+
+    def __init__(self, canvas=canvas, game_width=game_width, game_height=game_height,
+                 snake_size=snake_size, start_x=start_x, start_y=start_y):
+        self.snake_list = []
+        self.snake_size = snake_size
+        self.snake_x = start_x
+        self.snake_y = start_y
+        self.snake_x_nav = 0
+        self.snake_y_nav = 0
+        self.canvas = canvas
+        self.game_width = game_width
+        self.game_height = game_height
+        self.last_move = ['start']
+        self.game_running = True
+
+    # create and increasing the snake
+    def paint_item(self):
+
+        snake_elem = self.canvas.create_rectangle(self.snake_x * snake_item, self.snake_y * snake_item,
+                                                  self.snake_x * snake_item + snake_item,
+                                                  self.snake_y * snake_item + snake_item,
+                                                  fill=next_snake_color[0])
+        self.snake_list.append(snake_elem)
+        canvas.bind_all('<KeyPress-Left>', self.snake_move)
+        canvas.bind_all('<KeyPress-Right>', self.snake_move)
+        canvas.bind_all('<KeyPress-Up>', self.snake_move)
+        canvas.bind_all('<KeyPress-Down>', self.snake_move)
+        self.check_snake_items()
+
+    def check_snake_items(self):
+
+        if len(self.snake_list) >= self.snake_size:
+            print(self.snake_list[0])
+            temp_item = self.snake_list.pop(0)
+            self.canvas.delete(temp_item)
+
+    def check_border(self):
+
+        game_border_x = self.game_width // snake_item - 1
+        game_border_y = self.game_height // snake_item - 1
+        if self.snake_x > game_border_x or self.snake_x < 0 \
+                or self.snake_y > game_border_y or self.snake_y < 0:
+            self.game_over()
+
+    def game_over(self):
+        game = self.canvas.create_text(self.game_width / 2, self.game_height / 2,
+                                       text="GAME OVER!",
+                                       font="Arial 60",
+                                       fill="#ff0000")
+        self.game_running = False
+
+    def snake_move(self, event):
+        global difficulty
+        while self.game_running:
+            if event.keysym == 'Up':
+                if self.last_move[0] != 'Down':
+                    print(self.last_move[0])
+                    self.last_move[0] = 'Up'
+                    self.snake_x_nav = 0
+                    self.snake_y_nav = -1
+                    self.check_snake_items()
+            if event.keysym == 'Down':
+                if self.last_move[0] != 'Up':
+                    self.last_move[0] = 'Down'
+                    self.snake_x_nav = 0
+                    self.snake_y_nav = 1
+                    self.check_snake_items()
+            if event.keysym == 'Left':
+                if self.last_move[0] != 'Right':
+                    self.last_move[0] = 'Left'
+                    self.snake_x_nav = -1
+                    self.snake_y_nav = 0
+                    self.check_snake_items()
+            if event.keysym == 'Right':
+                if self.last_move[0] != 'Left':
+                    self.last_move[0] = 'Right'
+                    self.snake_x_nav = 1
+                    self.snake_y_nav = 0
+                    self.check_snake_items()
+            self.check_border()
+            self.snake_x = self.snake_x + self.snake_x_nav
+            self.snake_y = self.snake_y + self.snake_y_nav
+            self.paint_item()
+
+            # defines game speed
+            time.sleep(difficulty['Medium'])
+
+            tk.update_idletasks()
+            tk.update()
+
+            if apple.check_eat_apple(self.snake_x, self.snake_y):
+                apple.eat_apple()
+                self.snake_size = self.snake_size + 1
+                apple.__init__()
+
+
+snake = Snake(canvas=canvas, game_width=game_width, game_height=game_height,
+              snake_size=snake_size, start_x=start_x, start_y=start_y)
+
+snake.paint_item()
+
+apple = SnakeApple()
+
+tk.mainloop()
